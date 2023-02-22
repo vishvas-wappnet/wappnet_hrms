8b3cce68d9209c288aea9027ad31c268f3c88976
vishvas.wappnet <vishvas.wappnet@gmail.com>
2023-02-22 19:33:52 +0530

    working on forgot password

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CustomAuthController;
+use App\Http\Controllers\ForgotPasswordController;
 //Illuminate\Support\Facades\Auth;
 
 use Illuminate\Support\Facades\Auth;
@@ -23,18 +24,26 @@ Route::get('/', function () {
 
 
 
-Route::get('/indexx', function () {
-    return view('dashboard');
+Route::get('/forgot', function () {
+    return view('auth.forgot_password');
 });
 
-
+//login register routes
 Route::get('dashboard', [CustomAuthController::class, 'dashboard']); 
 Route::get('signout', [CustomAuthController::class, 'signOut'])->name('signout');
 Route::get('login', [CustomAuthController::class, 'index'])->name('login');
 Route::post('custom-login', [CustomAuthController::class, 'customLogin'])->name('login.custom'); 
 Route::get('registration', [CustomAuthController::class, 'registration'])->name('register-user');
-Route::post('custom-registration',[CustomAuthController::class, 'customRegistration'])->name('register.custom');//testing
+Route::post('custom-registration',[CustomAuthController::class, 'customRegistration'])->name('register.custom');
 
+
+
+//fotgot password
+
+Route::get('forget-password',[ForgotPasswordController::class, 'showForgetPasswordForm'])->name('forget.password.get');
+Route::post('forget-password',[ForgotPasswordController::class, 'submitForgetPasswordForm'])->name('forget.password.post'); 
+Route::get('reset-password/{token}',[ForgotPasswordController::class, 'showResetPasswordForm'])->name('reset.password.get');
+Route::post('reset-password', [ForgotPasswordController::class, 'submitResetPasswordForm'])->name('reset.password.post');
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
