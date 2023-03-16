70fdc7c70c210b015be1be463a9f662a044e4025
vishvas-wappnet <vishvas.wappnet@gmail.com>
2023-03-16 20:29:12 +0530

    [holiday enabled and disabled]   -add design   of toggle button  and change diplay data list page :- holiday.blade.php

--- resources/views/action/holiday_change_status.blade.php
+++ resources/views/action/holiday_change_status.blade.php
@@ -1,16 +1,12 @@
-
-   
+ 
 <head>
     <link href="https://gitcdn.github.io/bootstrap-toggle/2.2.2/css/bootstrap-toggle.min.css" rel="stylesheet">
     <script src="https://gitcdn.github.io/bootstrap-toggle/2.2.2/js/bootstrap-toggle.min.js"></script> 
 <head>
        <input data-id="{{$id}}" class="toggle-class" type="checkbox" data-onstyle="success" data-offstyle="danger"
         data-toggle="toggle" data-on="Enabled" data-off="InActive" {{$status ? 'checked' :'' }}>
-    
 <script>
-
         $(function() { 
-           
             $('.toggle-class').change(function() { 
             var status = $(this).prop('checked') == true ? 1 : 0;  
             var id = $(this).data('id');  
